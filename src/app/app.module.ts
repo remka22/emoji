d496704deb065151c9cdc788c7db8d3159a372ad
Remzik <90889988+Tasar232@users.noreply.github.com>
2023-05-13 17:43:36 +0800

    креате пайдс анд компонентс

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EmojiCardComponent } from './components/emoji-card/emoji-card.component';
+import { EmojiNavigateComponent } from './components/emoji-navigate/emoji-navigate.component';
+import { EmojiComponent } from './page/emoji/emoji.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EmojiCardComponent,
+    EmojiNavigateComponent,
+    EmojiComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EmojiComponent]
 })
 export class AppModule { }
